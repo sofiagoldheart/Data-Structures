b48fbd1af3be291795f1f2ec548e40b8274a763a
Sofia Ruvalcaba de la Noval <90588277+sofiagoldheart@users.noreply.github.com>
2022-02-11 08:32:29 -0500

    Update 5. apuntadores char.cpp

--- 5. apuntadores char.cpp
+++ 5. apuntadores char.cpp
@@ -11,3 +11,25 @@ char c1 [10] = {"SUPER_OK"};
       cout << *apc++ << endl;  
     }
 }
+
+/*	#include <iostream>
+	#include<stdio.h>
+	using namespace std;
+	
+	int tamcad(char cad[]);
+	
+	int main(){
+	char cad1[12] = {"DIPLODOCUS"};
+	cout << tamcad(cad1)<<endl;
+	}
+	
+	int tamcad(char cad[]){
+	char *pc1;
+	int i = 0;
+	
+	pc1 = cad;
+	while(*(pc1)!= '\0'){
+	pc1++;
+	
+	}
+}
